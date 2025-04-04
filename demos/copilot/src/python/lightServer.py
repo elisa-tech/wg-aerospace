94d1c7914f5d54df2cdb27a5b75fa519539f14ca
Paul Mani <paulmani88@yahoo.com>
2025-04-04 23:34:38 +0530

    Light server updated to allow configuration of a fixed delay or random delay via command line parameters Switch.py updated to toggle the switch continuously with a delay of 1 sec between toggles.
    Signed-off-by: Paul Mani <paulmani88@yahoo.com>

--- demos/copilot/src/python/lightServer.py
+++ demos/copilot/src/python/lightServer.py
@@ -8,16 +8,78 @@ from serverCommands import *
 import logger
 import appConfiguration
 import lightControl
+import random
+import argparse
 
 ServerCommand = appConfiguration.ServerConfiguration.ServerCommandSerializer
 
 LIGHT_ON_LOG_MESSAGE = 'lights: on'
 LIGHT_OFF_LOG_MESSAGE = 'lights: off'
 
+class ServerDelayType(Enum):
+    """List of the different possible Server Commands """
+    OFF = 0
+    FIXED = 1
+    RANDOM = 2
+
+class LightServerDelay(object):
+    """ Class that allows control of the time taken by the server to act on a received command."""
+    def __init__(self):
+        """ """
+
+        # Set up some default values
+        self.DelayControlSetting = ServerDelayType.OFF
+        self.DelayFixedMs = 200
+        self.RandomDelayLowerLimitMs = 100
+        self.RandomDelayUpperLimitMs = 700
+        self.RandomDelayIncrement = 100
+
+        self.LockObj = threading.Lock()
+        self.logger = logger.Logger().GetLogger()
+    
+    def SetDelayOff(self):
+        """ Turn off Delay"""
+        self.logger.info("Setting delay to default(100 ms)")
+        with self.LockObj:
+            self.DelayControlSetting = ServerDelayType.OFF
+        
+
+    def SetDelayFixed(self, delayTimeMs):
+        """ Set delay to a fixed value"""
+        self.logger.info(f"Setting delay to fixed - {delayTimeMs} ms")
+        with self.LockObj:
+            self.DelayControlSetting = ServerDelayType.FIXED
+            self.DelayFixedMs = delayTimeMs
+
+    def SetDelayRandom(self, delayTimeLowerLimitMs, delayTimeUpperLimitMs):
+        """ Turn off Delay"""
+        self.logger.info(f"Setting delay to Random - {delayTimeLowerLimitMs} ms to {delayTimeUpperLimitMs} ms")
+        with self.LockObj:
+            self.DelayControlSetting = ServerDelayType.RANDOM
+            self.RandomDelayLowerLimitMs = delayTimeLowerLimitMs
+            self.RandomDelayUpperLimitMs = delayTimeUpperLimitMs
+
+    def DoDelay(self):
+        """ """
+        delayTimeSec = 0.0
+
+        with self.LockObj:
+            # Even if the delay is set to off, add a delay for at least 100ms
+            if ServerDelayType.OFF == self.DelayControlSetting:
+                delayTimeSec = .1
+            elif ServerDelayType.FIXED == self.DelayControlSetting:
+                delayTimeSec = self.DelayFixedMs/1000
+            else:
+                # Return a random value in increments of 100 ms
+                delayTimeSec = random.randrange(self.RandomDelayLowerLimitMs, self.RandomDelayUpperLimitMs, self.RandomDelayIncrement)/1000
+            
+        time.sleep(delayTimeSec)
+
+
 class LightServer(threading.Thread):
     """Main thread that handles all server requests """
     
-    def __init__(self):
+    def __init__(self, delayController):
         """ """
         super().__init__()
         self.running = True
@@ -26,6 +88,7 @@ class LightServer(threading.Thread):
         self.sock.setblocking(False)
         self.sock.bind(self.serverAddress)
         self.light = lightControl.LightControl()
+        self.DelayController = delayController
         self.logger = logger.Logger().GetLogger()
 
     def run(self):
@@ -36,6 +99,10 @@ class LightServer(threading.Thread):
                 commandData, address = self.sock.recvfrom(4096)
                 # Parse the command
                 serverCommand = ServerCommand.DeSerialize(commandData)
+                
+                # Add a delay before commanding the light based on the configuration
+                self.DelayController.DoDelay()
+
                 if serverCommand.DeviceID == Devices.LIGHT:
                     if serverCommand.Command == ServerCommands.TURN_ON_DEVICE:
                         self.logger.info(LIGHT_ON_LOG_MESSAGE)
@@ -43,11 +110,13 @@ class LightServer(threading.Thread):
                     elif serverCommand.Command == ServerCommands.TURN_OFF_DEVICE:
                         self.logger.info(LIGHT_OFF_LOG_MESSAGE)
                         self.light.TurnOff()
+
             except BlockingIOError:
                 # This probably means that there is no data to read
-                pass
-
-            time.sleep(.01)
+                # Wait for a little while allow the server to respond to one command before
+                # moving on to the next and also reduce load on the CPU.
+                time.sleep(.01)
+            
         
     def stop(self):
         """ Stop the Light server"""
@@ -57,9 +126,10 @@ class LightServer(threading.Thread):
 
 class ServerMain:
     """Main application that is responsibel for kicking off the server thread and monitoring signals. """
-    def __init__(self):
+    def __init__(self, delayController):
         """ """
-        self.LightServer = LightServer()
+        self.DelayController = delayController
+        self.LightServer = LightServer(delayController)
         self.logger = logger.Logger().GetLogger()
     
     def SignalHandler(self, signum, frame):
@@ -90,6 +160,27 @@ class ServerMain:
                     
 if __name__ == "__main__":
 
+    # Parse Command line args
+    parser = argparse.ArgumentParser(description="Light Server program with configurable delay")
+
+    delay_group = parser.add_mutually_exclusive_group()
+    delay_group.add_argument('--delay-fixed', type=int, help="Fixed delay time in millisecond")
+    delay_group.add_argument('--delay-random', type=int, nargs=2, metavar = ('lowerLimit', 'upperLimit'), 
+                                help="Random delay lower limit and upper limit in millisecond(low high)")
+
+    cmdLineArgs = parser.parse_args()
+
+    delayController= LightServerDelay()
+
+    if cmdLineArgs.delay_fixed is not None:        
+        delayController.SetDelayFixed(cmdLineArgs.delay_fixed)
+    elif cmdLineArgs.delay_random is not None:
+        lowerLimit, upperLimit = cmdLineArgs.delay_random
+        delayController.SetDelayRandom(lowerLimit, upperLimit)
+    else:
+        delayController.SetDelayOff()
+
+        
     # Start the application
-    app = ServerMain()
+    app = ServerMain(delayController)
     app.Start()
