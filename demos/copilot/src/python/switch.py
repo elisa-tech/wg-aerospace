72eeed93773a45d43062159ea444ea68a2bc91ed
Paul Mani <paulmani88@yahoo.com>
2025-05-09 19:02:01 +0530

    Update to allow Light server and Swithch to be shutdown with SIGUSR1
    Signed-off-by: Paul Mani <paulmani88@yahoo.com>

--- demos/copilot/src/python/switch.py
+++ demos/copilot/src/python/switch.py
@@ -3,12 +3,12 @@ from serverCommands import *
 import socket
 import logger
 import time
-
+import signal
 
 ServerCommand = ServerCommandLightText
 SWITCH_ON_LOG_MESSAGE = 'switch: on'
 SWITCH_OFF_LOG_MESSAGE = 'switch: off'
-
+STOP_SWITCH = False
 Ip = "127.0.0.1"  
 Port = 10000 
 switchLogger = logger.Logger().GetLogger()
@@ -36,9 +36,18 @@ def TurnLightOff():
     SendUdpPacket(sres, Ip, Port)
     switchLogger.info(SWITCH_OFF_LOG_MESSAGE)
 
-if __name__ == "__main__":
+def SignalHandler(self, signum):
+    """ """
+    global STOP_SWITCH
+    switchLogger.info(f"Received signal {signum}. Shutting down the switch")
+    STOP_SWITCH = True
     
-    while True:
+
+if __name__ == "__main__":
+
+    signal.signal(signal.SIGUSR1, SignalHandler)
+    
+    while STOP_SWITCH == False:
         
         TurnLightOff()
         # Wait for a 1 sec before attempting to turn the switch on
