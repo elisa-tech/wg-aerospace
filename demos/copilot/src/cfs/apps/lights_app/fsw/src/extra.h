8f83d0ee886ae493f685e8000d14da6ea362a3d0
Ivan Perez <ivan.perezdominguez@nasa.gov>
2025-11-14 17:32:05 +0000

    Light and switch apps

---
+++ demos/copilot/src/cfs/apps/lights_app/fsw/src/extra.h
@@ -0,0 +1,15 @@
+#define LIGHTS_COMMAND_TOPIC_ID 0x1878
+#define LIGHTS_COMMAND CFE_PLATFORM_CMD_TOPICID_TO_MIDV(LIGHTS_COMMAND_TOPIC_ID)
+
+#define LIGHTS_STATUS_TOPIC_ID 0x1978
+#define LIGHTS_STATUS CFE_PLATFORM_CMD_TOPICID_TO_MIDV(LIGHTS_STATUS_TOPIC_ID)
+
+typedef struct lights_cmd_t {
+   CFE_MSG_CommandHeader_t CommandHeader;
+   bool payload;
+} lights_cmd_t;
+
+typedef struct lights_status_t {
+   CFE_MSG_TelemetryHeader_t TelemetryHeader;
+   bool payload;
+} lights_status_t;
