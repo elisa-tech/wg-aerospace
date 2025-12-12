e1450e4a9599e438004a677fa7b0399ea7135341
Salim Jalaleddine <salim.jalaleddine@boeing.com>
2025-12-12 09:17:16 -0700

    Refs #129, Make fixes to get cFS build working on QEMU.
    Signed-off-by: Salim Jalaleddine <salim.jalaleddine@boeing.com>

--- demos/copilot/src/cfs/elisa_usecase/extra.h
+++ demos/copilot/src/cfs/elisa_usecase/extra.h
@@ -1,17 +1,17 @@
 // SPDX-License-Identifier: MIT
 
 #define LIGHTS_COMMAND_TOPIC_ID 0x1878
-#define LIGHTS_COMMAND CFE_PLATFORM_CMD_TOPICID_TO_MIDV(LIGHT_COMMAND_TOPIC_ID)
+#define LIGHTS_COMMAND CFE_PLATFORM_CMD_TOPICID_TO_MIDV(LIGHTS_COMMAND_TOPIC_ID)
 
 #define LIGHTS_STATUS_TOPIC_ID 0x1978
-#define LIGHTS_STATUS CFE_PLATFORM_CMD_TOPICID_TO_MIDV(LIGHT_STATUS_TOPIC_ID)
+#define LIGHTS_STATUS CFE_PLATFORM_CMD_TOPICID_TO_MIDV(LIGHTS_STATUS_TOPIC_ID)
 
-typedef struct light_cmd_t {
+typedef struct lights_cmd_t {
    CFE_MSG_CommandHeader_t CmdHeader;
    bool payload;
-} light_cmd_t;
+} lights_cmd_t;
 
-typedef struct light_status_t {
+typedef struct lights_status_t {
    CFE_MSG_TelemetryHeader_t TlmHeader;
    bool payload;
-} light_status_t;
+} lights_status_t;
