6b35d276db0f189baa0051cba09a61fccef76041
Weber (US), Matthew L <matthew.l.weber3@boeing.com>
2026-02-07 05:13:16 +0000

    Check for docker before install
    - Also fixes a logic issue with sudo and set script
    
    Signed-off-by: Weber (US), Matthew L <matthew.l.weber3@boeing.com>

--- demos/env/setup-env.sh
+++ demos/env/setup-env.sh
@@ -4,7 +4,7 @@
 
 # Check if the script is running with root privileges
 if [[ "$EUID" -eq 0 ]]; then
-  echo "This script does not require running with 'sudo'."
+  echo "This script does not require running with 'sudo' or as root.  It does have commands in it which use sudo so there will be a password prompt."
   exit 1
 fi
 
@@ -14,17 +14,36 @@ if ! sudo -n true 2> /dev/null; then
     exit 1
 fi
 
+# Install Docker if missing
+if ! command -v /usr/bin/docker &> /dev/null; then
+    echo "Installing prerequisites ..."
+    {
+        sudo apt-get update &&
+            sudo apt-get install -y docker.io iptables
+    } || {
+        echo "Failed to setup Docker prerequisites"
+        exit 1
+    }
+else
+    echo "Docker already installed..."
+fi
+
+# Install Tool Dependencies
 echo "Installing prerequisites ..."
 {
     sudo apt-get update &&
-        sudo apt-get install -y containerd docker.io make ca-certificates passwd curl wslu iptables
+        sudo apt-get install -y make ca-certificates passwd curl
 } || {
-    echo "Failed to setup Docker prerequisites"
+    echo "Failed to setup Demo prerequisites"
     exit 1
 }
 
-# Setup user permissions. Ensure the actual user (not root) is used when the script is run with sudo initially to install docker
+# Setup user permissions for using Docker
+echo "Adding user to docker group ..."
 sudo usermod -aG docker "${SUDO_USER:-$USER}" || {
-   echo "Failed to configure user permissions"
-   exit 1
+    echo "Failed to configure user permissions"
+    exit 1
 }
+
+sudo systemctl status docker &> /dev/null || sudo systemctl restart docker
+sudo systemctl status docker | grep "Active"
