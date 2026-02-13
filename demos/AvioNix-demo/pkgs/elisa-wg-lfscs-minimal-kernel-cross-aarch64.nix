0c3b7af25578bb705f944f5e414c8063bbbe5279
Wanja Zaeske <wanja.zaeske@dlr.de>
2026-02-13 22:15:31 +0100

    feat: add elisa lfscs minimal kernel pkg
    This allows quickly measuring the SLoC for the given config.
    
    Signed-off-by: Wanja Zaeske <wanja.zaeske@dlr.de>

---
+++ demos/AvioNix-demo/pkgs/elisa-wg-lfscs-minimal-kernel-cross-aarch64.nix
@@ -0,0 +1,7 @@
+# SPDX-FileCopyrightText: 2026 wucke13
+#
+# SPDX-License-Identifier: Apache-2.0
+
+{ pkgsCross }:
+
+pkgsCross.aarch64-multiplatform.callPackage ./elisa-wg-lfscs-minimal-kernel.nix { }
