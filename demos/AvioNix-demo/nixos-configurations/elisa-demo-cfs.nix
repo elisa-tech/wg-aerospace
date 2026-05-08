db3472d5cd6560e0798e734dfc183b10fe2af07b
wucke13 <wucke13+github@gmail.com>
2026-05-08 16:49:32 +0200

    Fix cFS crash in AvioNix, add cFS debug setup
    Two kernel config options where missing, as we found out in a fun round
    of peer-debugging with GDB.
    
    Signed-off-by: wucke13 <wucke13+github@gmail.com>

--- demos/AvioNix-demo/nixos-configurations/elisa-demo-cfs.nix
+++ demos/AvioNix-demo/nixos-configurations/elisa-demo-cfs.nix
@@ -22,6 +22,18 @@
       pkgs.callPackage (flakeRoot + "/pkgs/minimal-linux-kernel.nix") { }
     );
 
+    boot.kernelPatches = [
+      {
+        name = "enable-cFS-dependencies";
+        patch = null;
+        structuredExtraConfig = {
+          # These are needed for cFS
+          POSIX_MQUEUE = lib.kernel.yes;
+          SYSVIPC = lib.kernel.yes;
+        };
+      }
+    ];
+
     zorn.boot.kernelDebugging.enable = true; # enable gdb launcher script generation
     zorn.boot.kernelDebugging.enableKernelDebugConfigOptions = true; # compile kernel with debugging aids
     zorn.boot.standaloneInitramdisk.enable = true; # enable creation of a standalone initramdisk
@@ -62,6 +74,7 @@
       pkgs.zstd
 
       # debugging aids
+      pkgs.gdb
       # pkgs.iproute2
       # pkgs.netcat
       # pkgs.socat
@@ -69,6 +82,22 @@
 
       # ELISA demo software
       pkgs.elisa-demo-python-light
+
+      # add source to rootfs
+      (pkgs.writeShellApplication {
+        # break exit
+        # up
+        name = "debug-cfs";
+        text = ''
+          set -x
+          CPU="''${1:-cpu1}"
+
+          pushd -- "${pkgs.enableDebugging pkgs.elisa-demo-cfs}/exe/$CPU"
+          gdb --eval-command='set substitute-path /build/src/cfs ${pkgs.srcOnly pkgs.elisa-demo-cfs}' \
+            "core-$CPU"
+        '';
+      })
+
     ];
 
     # use this to add packages to the early boot stage
@@ -86,7 +115,7 @@
     # add our applications
     systemd.services.elisa-demo-cfs =
       let
-        cfsPkg = pkgs.elisa-demo-cfs;
+        cfsPkg = pkgs.enableDebugging pkgs.elisa-demo-cfs;
       in
       {
         wantedBy = [ "multi-user.target" ];
